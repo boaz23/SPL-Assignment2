3959438d56ca4e666d41208d71ab82d742494a45
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2019-12-17 14:53:03 +0200

    changed gadget available event fields

--- src/main/java/bgu/spl/mics/application/messages/eventArgs/GadgetAvailableEventArgs.java
+++ src/main/java/bgu/spl/mics/application/messages/eventArgs/GadgetAvailableEventArgs.java
@@ -1,13 +1,22 @@
 package bgu.spl.mics.application.messages.eventArgs;
 
 public class GadgetAvailableEventArgs {
+    private final String gadget;
     private boolean isAvailable;
 
-    public GadgetAvailableEventArgs(boolean isAvailable) {
-        this.isAvailable = isAvailable;
+    public GadgetAvailableEventArgs(String gadget) {
+        this.gadget = gadget;
+    }
+
+    public String getGadget() {
+        return gadget;
     }
 
     public boolean isAvailable() {
         return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
     }
 }
